e185a7706581549d919a1f8c8bfbe0a9dc5abec7
smuhammadkahaf <smuhammadkahaf@gmail.com>
2024-07-01 07:38:16 +0500

    Menu with animation

--- tickcross_.cpp
+++ tickcross_.cpp
@@ -14,6 +14,7 @@ int  check(char  [3][3]);
 void reset(char[3][3]);
 int  back();
 void exchange(string*,string*,int*,int*);
+void animation();
 
 //main function
 int main()
@@ -27,7 +28,7 @@ int main()
     homescreen:
     point1=0;
     point2=0;
-    reset(game);
+    animation();
     choice = menu();
 
     switch(choice)
@@ -80,7 +81,7 @@ int main()
             }
             cout<<endl<<"<Wish You Guys The Best>"<<endl;
             Sleep(1000);
-            restart:
+            restart://restart
             reset(game);
             u=0;
             for (int i=0;i<9;)
@@ -188,7 +189,7 @@ int main()
 //this function is the interface of main menu + selecting choice
 char menu(void)
 {   char menu;
-    cout<<"\t<<Tic-Tac-Toe>>"<<endl;
+    cout<<endl;
     cout<<"Menu:"<<endl;
     cout<<"1. How to play (strongly recommended)"<<endl;
     cout<<"2. Start game"<<endl;
@@ -218,7 +219,7 @@ void help(void)
     char waste;
     char eg [3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
     system ("cls");
-    cout<<"< How to play game >"<<endl;
+    cout<<endl;
     cout<<endl<<"---> If you want to restart the game in your turn then press r";
     cout<<endl<<"---> After restarting game will continue with same player and their current score";
     cout<<endl<<"---> how to mark your turn?"<<endl;
@@ -496,4 +497,20 @@ void exchange(string* p1,string* p2, int* point1,int* point2)
     temp   =*point1;
     *point1=*point2;
     *point2=temp;
+}
+void animation()
+{
+	cout<<"<";  Sleep(100);
+	cout<<"T";  Sleep(100);
+	cout<<"i";  Sleep(100);
+	cout<<"c";  Sleep(100);
+	cout<<" ";  Sleep(100);
+	cout<<"T";  Sleep(100);
+	cout<<"a";  Sleep(100);
+	cout<<"c";  Sleep(100);
+	cout<<" ";  Sleep(100);
+	cout<<"T";  Sleep(100);
+	cout<<"o";  Sleep(100);
+	cout<<"e";  Sleep(100);
+	cout<<">";  Sleep(100);
 }
